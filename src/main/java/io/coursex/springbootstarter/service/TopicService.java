2c88985d6ce9004cf475228c73b1981095c60a9b
Ankit Singh <ankitsingh4154@gmail.com>
2021-09-05 13:31:45 +0530

    Added addTopic for POST requests

--- src/main/java/io/coursex/springbootstarter/service/TopicService.java
+++ src/main/java/io/coursex/springbootstarter/service/TopicService.java
@@ -1,5 +1,6 @@
 package io.coursex.springbootstarter.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,9 +11,9 @@ import io.coursex.springbootstarter.model.Topic;
 @Service
 public class TopicService {
 
-	List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Boot", "Spring Boot Description"),
+	List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("spring", "Spring Boot", "Spring Boot Description"),
 			new Topic("java", "Core Java", "Core Java Description"),
-			new Topic("javascript", "Javascript", "Javascript Description"));
+			new Topic("javascript", "Javascript", "Javascript Description")));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -20,6 +21,10 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
