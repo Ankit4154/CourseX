057e26d11a04c329174f609f29f85d6e4a80ff47
Ankit Singh <ankitsingh4154@gmail.com>
2021-09-06 13:00:56 +0530

    Refractoring

--- src/main/java/io/coursex/springbootstarter/controller/TopicController.java
+++ src/main/java/io/coursex/springbootstarter/controller/TopicController.java
@@ -1,7 +1,6 @@
 package io.coursex.springbootstarter.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,30 +13,31 @@ import io.coursex.springbootstarter.service.TopicService;
 
 @RestController
 public class TopicController {
-	
+
 	@Autowired
 	private TopicService topicService;
-	
+
 	@RequestMapping("/topics")
-	public List<Topic> getTopics(){
+	public List<Topic> getTopics() {
 		return topicService.getAllTopics();
 	}
+
 	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
 
-	@RequestMapping(method=RequestMethod.POST ,value="/topics")
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
-	
-	@RequestMapping(method=RequestMethod.PUT ,value="/topics/{id}")
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
 	public List<Topic> updateTopic(@RequestBody Topic topic, @PathVariable String id) {
 		return topicService.updateTopic(topic, id);
 	}
-	
-	@RequestMapping(method=RequestMethod.DELETE ,value="/topics/{id}")
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
 	public List<Topic> deleteTopic(@PathVariable String id) {
 		return topicService.deleteTopic(id);
 	}
