48a9afdd0203d0b002471fc465c9e87c3d2b66c5
Ankit Singh <ankitsingh4154@gmail.com>
2021-09-26 14:02:44 +0530

    Testing git auth

--- configserver/src/main/java/io/course/configserver/CourseConfigServerApplication.java
+++ configserver/src/main/java/io/course/configserver/CourseConfigServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseConfigServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This is test");
 		SpringApplication.run(CourseConfigServerApplication.class, args);
 	}
 
