da9df525b9c56585047204e302f4c1382aa939f2
Ankit Singh <ankitsingh4154@gmail.com>
2021-09-26 14:05:15 +0530

    Enabled config server

--- configserver/src/main/java/io/course/configserver/CourseConfigServerApplication.java
+++ configserver/src/main/java/io/course/configserver/CourseConfigServerApplication.java
@@ -2,12 +2,13 @@ package io.course.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CourseConfigServerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("This is test");
 		SpringApplication.run(CourseConfigServerApplication.class, args);
 	}
 
