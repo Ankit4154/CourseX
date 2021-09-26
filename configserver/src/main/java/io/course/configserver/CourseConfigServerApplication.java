5a96a7570e7f78063d6698d532dd8257feb9ce44
Ankit Singh <ankitsingh4154@gmail.com>
2021-09-26 13:32:08 +0530

    Spring Cloud Config Server for centralized configuration properties to all microservices

---
+++ configserver/src/main/java/io/course/configserver/CourseConfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.course.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseConfigServerApplication.class, args);
+	}
+
+}
---
+++ configserver/src/test/java/io/course/configserver/CourseConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.course.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
