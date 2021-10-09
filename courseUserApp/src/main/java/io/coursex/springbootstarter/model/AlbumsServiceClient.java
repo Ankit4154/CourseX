e4c4d4189b0748336294e318847b3c6dc690ab10
Ankit Singh <ankitsingh4154@gmail.com>
2021-10-09 20:36:58 +0530

    Not adding Hysterix or Resilience circuit breaker for albums service fallback mechanism

--- courseUserApp/src/main/java/io/coursex/springbootstarter/model/AlbumsServiceClient.java
+++ courseUserApp/src/main/java/io/coursex/springbootstarter/model/AlbumsServiceClient.java
@@ -1,16 +1,39 @@
 package io.coursex.springbootstarter.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="albums")
+@FeignClient(name = "albums")//, fallback = AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
-	// @GetMapping("/users/{id}/albums")
 	// Changed uri to ss for raising FeignException
-	@GetMapping("/users/{id}/albumss")
+	// @GetMapping("/users/{id}/albumss")
+	@GetMapping("/users/{id}/albums")
 	public List<AlbumResponse> getAlbums(@PathVariable String id);
 }
+
+/*
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+
+	@Override
+	public List<AlbumResponse> getAlbums(String id) {
+		List<AlbumResponse> returnValue = new ArrayList<>();
+        
+		AlbumResponse albumEntity = new AlbumResponse();
+        albumEntity.setUserId(id);
+        albumEntity.setAlbumId("Fallback AnkitId");
+        albumEntity.setDescription("Fallback Ankit description");
+        albumEntity.setId(1L);
+        albumEntity.setName("Fallback Ankit album 1 name");
+        returnValue.add(albumEntity);
+        
+        return returnValue;
+	}
+}
+*/
